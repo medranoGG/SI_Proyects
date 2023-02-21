87c1f610a85b131a6023f75b589d6cacb56c58df
2k9Pablo <44757561+2k9Pablo@users.noreply.github.com>
2023-02-21 22:18:32 +0100

    Json Sqlite3 DB Dump
    Created the functions to dump the .json file "devices.json" into the Sqlite3 db "base.db"

--- main.py
+++ main.py
@@ -1,25 +1,100 @@
 import sqlite3 as sq
 import pandas as pd
 import json
-con = sq.connect('base.db')
 
-c = con.cursor()
+conn = sq.connect('base.db')
 
-##Campos del csv timestamp,sid,msg,clasificacion,prioridad,protocolo,origen,destino,puerto
+curs = conn.cursor()
 
-##c.execute('''CREATE TABLE alerts (timestamp text,sid int,msg text,clasificacion text,prioridad int,protocolo text,origen text,destino text, puerto int)''')
+#CREAR TABLAS DEVICES, RESPONSABLE & ANALISIS
+create_table_responsable_query = '''
+    CREATE TABLE IF NOT EXISTS responsables (
+        nombre TEXT,
+        telefono INTEGER NOT NULL,
+        rol TEXT NOT NULL,
+        
+        PRIMARY KEY (nombre)
+    )
+'''
+curs.execute(create_table_responsable_query)
 
+create_table_devices_query = '''
+    CREATE TABLE IF NOT EXISTS devices (
+        id INTEGER NOT NULL,
+        ip TEXT NOT NULL,
+        localizacion TEXT,
+        responsable TEXT,
 
-##alerts = pd.read_csv('alerts.csv')
-# write the data to a sqlite table
-##alerts.to_sql('alerts', con, if_exists='append', index = False)
+        PRIMARY KEY (ip),
+        FOREIGN KEY (responsable)
+            REFERENCES responsables(nombre) ON DELETE SET NULL
+    )
+'''
+curs.execute(create_table_devices_query)
+ 
+create_table_analisis_query = '''
+    CREATE TABLE IF NOT EXISTS analisis (
+        ip TEXT NOT NULL,
+        puertos TEXT,
+        servicios INTEGER,
+        servicios_vulnerables INTEGER,
+        vulnerabilidades INTEGER,
+        
+        FOREIGN KEY (ip)
+            REFERENCES devices(ip) ON DELETE SET NULL
+    )
+'''
+curs.execute(create_table_analisis_query)
 
-with open('test.json') as f:
-    data=json.load(f)
+##Campos del csv timestamp, sid, msg, clasificacion, prioridad, protocolo, origen, destino, puerto
 
-##df = pd.DataFrame(data)
-##df.to_sql("devices",con)
-datosprueba=pd.json_normalize(data)
-print(datosprueba)
-df=pd.DataFrame(datosprueba)
-df.to_sql("devices",con)
+create_table_alerts_query = '''
+    CREATE TABLE IF NOT EXISTS alerts (
+        timestamp TEXT NOT NULL,
+        sid INTEGER,
+        message TEXT,
+        clasificacion TEXT,
+        prioridad INTEGER,
+        protocolo TEXT,
+        origen TEXT,
+        destino TEXT,
+        puerto INTEGER,
+
+        PRIMARY KEY (sid)
+    )
+'''
+curs.execute(create_table_alerts_query)
+
+#CSV
+print("Started reading the csv file")
+alerts = pd.read_csv('alerts.csv')
+#Write the data to a sqlite table
+alerts.to_sql('alerts', conn, if_exists='append', index = False)
+
+#JSON
+print("Started reading the json file")
+
+with open('devices.json') as d:
+    devices = json.load(d)
+
+for device in devices:
+    id = device['id']
+    ip = device['ip']
+    localizacion = device['localizacion']
+    responsable_nombre = device['responsable']['nombre']
+    responsable_telefono = device['responsable']['telefono']
+    responsable_rol = device['responsable']['rol']
+    puertos_abiertos = device['analisis']['puertos_abiertos']
+    servicios = device['analisis']['servicios']
+    servicios_inseguros = device['analisis']['servicios_inseguros']
+    vulnerabilidades_detectadas = device['analisis']['vulnerabilidades_detectadas']
+
+    print(id, ip, localizacion, responsable_nombre, responsable_telefono, responsable_rol, puertos_abiertos, servicios, servicios_inseguros, vulnerabilidades_detectadas)
+
+    # Insert the data into the database
+    curs.execute("INSERT INTO devices (id, ip, localizacion, responsable) VALUES (?, ?, ?, ?)", (id, ip, localizacion, responsable_nombre))
+    curs.execute("INSERT OR IGNORE INTO responsables (nombre, telefono, rol) VALUES (?, ?, ?)", (responsable_nombre, responsable_telefono, responsable_rol))
+    curs.execute("INSERT INTO analisis (ip, puertos, servicios, servicios_vulnerables, vulnerabilidades) VALUES (?, ?, ?, ?, ?)", (ip, json.dumps(puertos_abiertos), servicios, servicios_inseguros, vulnerabilidades_detectadas))
+
+    conn.commit()
+    conn.close()
