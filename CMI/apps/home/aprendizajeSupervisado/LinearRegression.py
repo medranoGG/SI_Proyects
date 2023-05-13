069304a87f94f2e7b5d27499485cb8fdf9fed2e5
P4bl0rc8 <pabloredondoca8@gmail.com>
2023-05-13 04:38:41 -0400

    Regresion Linear

--- CMI/apps/home/aprendizajeSupervisado/LinearRegression.py
+++ CMI/apps/home/aprendizajeSupervisado/LinearRegression.py
@@ -2,29 +2,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+import json
 
-# Load the diabetes dataset
-diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
-# Use only one feature
-diabetes_X = diabetes_X[:, np.newaxis, 2]
-# Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
-# Split the targets into training/testing sets
-diabetes_y_train = diabetes_y[:-20]
-diabetes_y_test = diabetes_y[-20:]
-# Create linear regression object
+with open("dump/devices_IA_clases.json", "r") as f:
+    trainDevices = json.load(f)
+
+with open("dump/devices_IA_predecir_v2.json", "r") as f:
+    testDevices = json.load(f)
+
+xTrain = []
+yTrain = []
+xTest = []
+yTest = []
+yPredict = []
+for i in trainDevices:
+    if i['servicios'] == 0:
+        xTrain.append([0])
+    else:
+        xTrain.append([i['servicios_inseguros']/i['servicios']])
+    yTrain.append([i['peligroso']])
+
+for i in testDevices:
+    if i['servicios'] == 0:
+        xTest.append([0])
+    else:
+        xTest.append([i['servicios_inseguros'] / i['servicios']])
+    yTest.append([i['peligroso']])
+
+
 regr = linear_model.LinearRegression()
-# Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
-print(regr.coef_)
-# Make predictions using the testing set
-diabetes_y_pred = regr.predict(diabetes_X_test)
-# The mean squared error
-print("Mean squared error: %.2f" % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+regr.fit(xTrain,yTrain)
+
+yPredict = regr.predict(xTest)
+print("Mean squared error: %.2f" % mean_squared_error(yTest, yPredict))
 # Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
-plt.plot(diabetes_X_test, diabetes_y_pred, color="blue", linewidth=3)
+plt.scatter(xTest, yTest, color="black")
+plt.plot(xTest, yPredict, color="blue", linewidth=3)
 plt.xticks(())
 plt.yticks(())
 plt.show()
